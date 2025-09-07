914a964f12bdf30a04a0b58528135b420fd84df8
Tobias Heidler <tobias@heidler.ovh>
2025-09-07 08:42:43 +0200

    Use the homepage field only if there is a dedicated website. Otherwise, prefer the  field.

--- package-build.sh
+++ package-build.sh
@@ -1,8 +1,8 @@
 #/bin/zsh
 ./build.sh
-mkdir -p ../packages/packages/preview/typst-comitia/0.1.0
-cp -r ./typst-package/* ../packages/packages/preview/typst-comitia/0.1.0/
-cp -r ./LICENSE ../packages/packages/preview/typst-comitia/0.1.0/
-cp -r ./README.md ../packages/packages/preview/typst-comitia/0.1.0/
-sed -i '' 's|#import "./lib.typ"|#import "@preview/comitia:0.1.0"|' ../packages/packages/preview/typst-comitia/0.1.0/example.typ
-mv ../packages/packages/preview/typst-comitia/0.1.0/example.typ ../packages/packages/preview/typst-comitia/0.1.0/examples/example.typ
+mkdir -p ../packages/packages/preview/comitia/0.1.0
+cp -r ./typst-package/* ../packages/packages/preview/comitia/0.1.0/
+cp -r ./LICENSE ../packages/packages/preview/comitia/0.1.0/
+cp -r ./README.md ../packages/packages/preview/comitia/0.1.0/
+sed -i '' 's|#import "./lib.typ"|#import "@preview/comitia:0.1.0"|' ../packages/packages/preview/comitia/0.1.0/example.typ
+mv ../packages/packages/preview/comitia/0.1.0/example.typ ../packages/packages/preview/comitia/0.1.0/examples/example.typ
